8ec6f1024ada2ae30948be0c89a93d7ed70daf66
Szabó Márton <mart.szaboo@gmail.com>
2020-05-17 14:24:18 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+//Tutorial betöltése
+
+const loadTutorial = (openingName) => {
+  let openingTextNode = document.createElement("p");
+  openingTextNode.textContent = openingName;
+  document.getElementById("list").appendChild(openingTextNode);
+};
+
+//-----------------------
+
+//Detail mutatása-eltüntetése
+
+const showDetail = (children) => {
+  let detail;
+  for (let i = 0; i < children.length; i++) {
+    if (children[i].classList.contains("detail")) {
+      detail = children[i];
+    }
+  }
+  if (detail.classList.contains("show")) {
+    detail.classList.remove("show");
+  } else {
+    detail.classList.add("show");
+  }
+};
+//-------------------------------//
+
+// 'LI' elements //
+
+const openings = document.getElementsByTagName("li");
+for (let i = 0; i < openings.length; i++) {
+  let opening = openings[i];
+  opening.addEventListener("click", (e) => {
+    //Behozza-eltünteti a detaileket//
+    showDetail(opening.children);
+    //--------------------------//
+
+    //Betölti a tutorialt//
+    loadTutorial(e.target.textContent);
+
+    // console.log(e.target.textContent);
+  });
+}
+
+//----------------------------------------//
